012d9a9aed131c6ec287151fbc1b02e5a3fadca3
Luiz Fernando Nunes <luiz_fernando_nunes@hotmail.com>
2018-10-02 14:22:06 -0300

    Upload Files

---
+++ SinglecOne/start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+conky -q -c ~/.conky/SinglecOne/conkyrc1 &
+conky -q -c ~/.conky/SinglecOne/conkyrc2 &
+conky -q -c ~/.conky/SinglecOne/conkyrc3 &
+conky -q -c ~/.conky/SinglecOne/conkyrc4 &
+
+exit
+
