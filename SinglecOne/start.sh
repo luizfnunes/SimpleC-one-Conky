c8fce2ce9a1f0319ed978fbb0d24ac2bc098541e
Luiz Fernando Nunes <luiz_fernando_nunes@hotmail.com>
2018-10-02 17:46:22 -0300

    Delete File

--- SinglecOne/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-conky -q -c ~/.conky/SinglecOne/conkyrc1 &
-conky -q -c ~/.conky/SinglecOne/conkyrc2 &
-conky -q -c ~/.conky/SinglecOne/conkyrc3 &
-conky -q -c ~/.conky/SinglecOne/conkyrc4 &
-
-exit
-
