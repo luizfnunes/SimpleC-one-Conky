8c83a2d157b2869bdbc5678a24892ef8beba80bd
Luiz Fernando Nunes <luiz_fernando_nunes@hotmail.com>
2018-10-02 17:47:57 -0300

    Add Folder

---
+++ SimplecOne/start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+conky -q -c ~/.conky/SimplecOne/conkyrc1 &
+conky -q -c ~/.conky/SimplecOne/conkyrc2 &
+conky -q -c ~/.conky/SimplecOne/conkyrc3 &
+conky -q -c ~/.conky/SimplecOne/conkyrc4 &
+
+exit
+
